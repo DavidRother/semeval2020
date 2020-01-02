4bbef71480de4443761e1788c0cef0948555e5ef
David Rother <david@edv-drucksysteme.de>
2020-01-02 01:38:13 +0100

    new pipeline for embedding generation

---
+++ semeval2020/main/embedding_computation.py
@@ -0,0 +1,46 @@
+from semeval2020.model.bertwrapper import BertWrapper
+from semeval2020.model.dataloader import Dataloader
+from semeval2020.model import preprocessing
+
+import torch
+
+
+########################################
+#  Config Parameter ####################
+########################################
+
+language = 'german'
+corpus = "corpus2"
+
+base_path = "../../trial_data_public/"
+model_string = "bert-base-multilingual-cased"
+
+output_path = "../semeval2020/embedding_data/"
+
+max_length_sentence = 100
+padding_length = 128
+
+########################################
+#  Code ################################
+########################################
+
+data_loader = Dataloader(base_path, language=language, corpus=corpus)
+bert_model = BertWrapper(model_string=model_string)
+
+data_loader.sentences = preprocessing.sanitized_sentences(data_loader.sentences, max_len=max_length_sentence)
+data_loader.sentences = preprocessing.filter_for_words(data_loader.sentences, data_loader.target_words)
+tokenized_target_sentences = bert_model.tokenize_sentences(data_loader.sentences, data_loader.target_words)
+target_embeddings_dict = {target: [] for target in data_loader.target_words}
+
+for tokenized_sentence, target_word_idx_dict in tokenized_target_sentences:
+    input_ids = bert_model.get_tokenized_input_ids(tokenized_sentence, padding_length=padding_length)
+    attention_mask = bert_model.get_attention_mask(input_ids)
+
+    input_id_tensor = torch.tensor([input_ids])
+    attention_mask_tensor = torch.tensor([attention_mask])
+    target_embeddings = bert_model.compute_embeddings(input_id_tensor, attention_mask, target_word_idx_dict)
+    for target, embeddings in target_embeddings.items():
+        target_embeddings_dict[target].extend(embeddings)
+
+
+
--- semeval2020/model/preprocessing.py
+++ semeval2020/model/preprocessing.py
@@ -1,26 +1,9 @@
-from pytorch_pretrained_bert import BertTokenizer
-from keras.preprocessing.sequence import pad_sequences
 
 
 def sanitized_sentences(sentences, max_len=100):
     for sentence in sentences:
         for split_sentence in __split_sentence(sentence, max_len):
             yield split_sentence
-
-
-def tokenize_sentences(sentences, tokenizer_model='bert-base-multilingual-cased',
-                       word_to_index=None, padding_length=128):
-    word_to_index = word_to_index or []
-    tokenizer = BertTokenizer.from_pretrained(tokenizer_model)
-    tokenized_target_words = {word: tokenizer.tokenize(word) for word in word_to_index}
-    for sentence in sentences:
-        tokenized_text = tokenizer.tokenize(' '.join(["[CLS]"] + sentence + ["[SEP]"]))
-        word_to_idx_dict = {word: [(i, i+len(tokenized_target_words[word])) for i, tok in enumerate(tokenized_text)
-                                   if tokenized_text[i: i+len(tokenized_target_words[word])] ==
-                                   tokenized_target_words[word]] for word in word_to_index}
-        input_ids = pad_sequences([tokenizer.convert_tokens_to_ids(tokenized_text)], maxlen=padding_length,
-                                  dtype="long", truncating="post", padding="post")[0]
-        attention_mask = []
 
 
 def __split_sentence(sentence, max_len):
@@ -30,4 +13,10 @@ def __split_sentence(sentence, max_len):
         yield sentence[:max_len]
         for sentence_split in __split_sentence(sentence[max_len:], max_len):
             yield sentence_split
+
+
+def filter_for_words(sentences, target_words):
+    for sentence in sentences:
+        if any((target for target in target_words if target in sentence)):
+            yield sentence
 
